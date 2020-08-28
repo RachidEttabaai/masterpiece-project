ffab4f9b0151f420116f88a6a02c534c0e1783df
Rachid ETTABAAI <rachid.ettabaai.simplon@gmail.com>
2020-08-28 13:52:01 +0200

    showing global news for covid with newsapi on JS in the home page v3

--- assets/js/news.js
+++ assets/js/news.js
@@ -6,6 +6,16 @@ $(document).ready(function() {
 
     let countrySelected = $("#select-country option:selected").val();
 
-    getJsonData(countrySelected)
+    getJsonData(countrySelected);
+
+    $("#select-country").change(function() {
+
+        $(this).find("option:selected").each(function() {
+
+            $("#news").empty();
+            getJsonData($(this).val());
+        });
+
+    }).change();
 
 });
