cce5c2d9c926713865513f7f09b4af02981ebb46
Rachid ETTABAAI <rachid.ettabaai.simplon@gmail.com>
2020-09-04 13:30:01 +0200

    add unit test for the countries' list in the API

--- tests/Api/ApiTest.php
+++ tests/Api/ApiTest.php
@@ -32,7 +32,7 @@ class ApiTest extends TestCase
         $this->assertTrue($response->hasHeader("Content-Type"));
     }
 
-    public function testGetContentApi(): void
+    public function testSummaryGetContentApi(): void
     {
         $response = $this->client->request("GET", "https://api.covid19api.com/summary");
 
@@ -42,7 +42,44 @@ class ApiTest extends TestCase
         $this->assertEquals("application/json; charset=UTF-8", $typeContent);
 
         $data = json_decode($response->getBody(), true);
+        $this->assertIsArray($data);
+
         $this->assertArrayHasKey("Countries", $data);
+
+        $this->assertIsArray($data["Countries"]);
+
+        // foreach ($data["Countries"] as $countrydata) {
+        //     $this->assertArrayHasKey("Country", $countrydata);
+        //     $this->assertArrayHasKey("Slug", $countrydata);
+        //     $this->assertArrayHasKey("CountryCode", $countrydata);
+        // }
+
         $this->assertArrayHasKey("Global", $data);
+
+        $this->assertIsArray($data["Global"]);
+
+        // $this->assertArrayHasKey("NewConfirmed", $data["Global"]);
+        // $this->assertArrayHasKey("TotalConfirmed", $data["Global"]);
+        // $this->assertArrayHasKey("NewDeaths", $data["Global"]);
+        // $this->assertArrayHasKey("TotalDeaths", $data["Global"]);
+        // $this->assertArrayHasKey("NewRecovered", $data["Global"]);
+        // $this->assertArrayHasKey("TotalRecovered", $data["Global"]);
+    }
+
+    public function testCountriesGetContentApi(): void
+    {
+        $response = $this->client->request("GET", "https://api.covid19api.com/countries");
+
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $datas = json_decode($response->getBody(), true);
+
+        $this->assertIsArray($datas);
+
+        // foreach ($datas as $data) {
+        //     $this->assertArrayHasKey("Country", $data);
+        //     $this->assertArrayHasKey("Slug", $data);
+        //     $this->assertArrayHasKey("ISO2", $data);
+        // }
     }
 }
