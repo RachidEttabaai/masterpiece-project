e8fc8f91a2caad0c0bdbd2cc53adf452f81c9930
Rachid ETTABAAI <rachid.ettabaai.simplon@gmail.com>
2020-09-10 09:53:31 +0200

    add router to the dependecy injection container

--- config/config.php
+++ config/config.php
@@ -1,14 +1,20 @@
 <?php
 
+use App\Init\Init;
 use App\Map\MapModule;
+use App\Router\Router;
 use App\Home\HomeModule;
 use function DI\factory;
 use App\About\AboutModule;
 use App\Error\ErrorModule;
-use App\ApiData\ApiDataModule;
 use App\Module\ModuleFactory;
+use App\Router\RouterFactory;
+use App\ApiData\ApiDataModule;
+use function Http\Response\send;
+use GuzzleHttp\Psr7\ServerRequest;
 use App\Renderer\RendererInterface;
 use App\Renderer\TwigRendererFactory;
+use Psr\Container\ContainerInterface;
 
 return [
     "modules_controller" => [
@@ -21,5 +27,12 @@ return [
     "template_path" => dirname(__DIR__) . DIRECTORY_SEPARATOR . "templates",
     "error.path" => dirname(__DIR__) . DIRECTORY_SEPARATOR . "templates" . DIRECTORY_SEPARATOR . "errors",
     RendererInterface::class => factory([TwigRendererFactory::class, "__invoke"]),
-    "listmodules" => factory([ModuleFactory::class, "__invoke"])
+    "listmodules" => factory([ModuleFactory::class, "__invoke"]),
+    "init" => function (ContainerInterface $container) {
+        $init = new Init($container);
+        $response = $init->run(ServerRequest::fromGlobals());
+        send($response);
+    },
+    "router" => new Router(),
+    Router::class => factory([RouterFactory::class, "__invoke"])
 ];
